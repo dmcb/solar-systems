b3080f4fa5a170bfbea5e6107aa4b12f1c3067b0
Derek McBurney <derekmcburney@Dereks-Work-MBP.localdomain>
2022-10-24 21:09:01 -0600

    Adds random orbital start position

--- js/planet.js
+++ js/planet.js
@@ -1,16 +1,21 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.145.0/three.module.js';
 
 class Planet {
-  constructor(colour, size, daysInAYear, orbitDistance) {
+  constructor(colour, size, daysInAYear, orbitDistance, orbitEccentricity) {
     this.orbitPosition = 0;
     this.colour = colour;
     this.size = size;
     this.daysInAYear = daysInAYear;
     this.orbitDistance = orbitDistance;
+    this.orbitEccentricity = orbitEccentricity;
+
+    this.orbitalPosition = Math.random()*360;
+
     if (!this.colour) this.colour = new THREE.Color( Math.random()*0xffffff );
     if (!this.size) this.size = Math.random()*6;
     if (!this.daysInAYear) this.daysInAYear = 1;
     if (!this.orbitDistance) this.orbitDistance = -10 + Math.random()*-90;
+    if (!this.orbitEccentricity) this.orbitEccentricity = Math.random()*0.1;
 
     const geometry = new THREE.SphereGeometry( this.size );
     const material = new THREE.MeshLambertMaterial( { color: this.colour } );
@@ -25,13 +30,13 @@ class Planet {
   travel() {
     this.sphere.rotation.z += this.daysInAYear*0.01;
     let x = this.orbitDistance;
-    let y = this.orbitDistance;
+    let y = this.orbitDistance * Math.sqrt(1.0 - Math.pow(this.orbitEccentricity, 1));
     let z = Math.sin(0) * this.orbitDistance;
-    this.orbitPosition += 0.005;
+    this.orbitalPosition += 0.005;
 
-    this.sphere.position.x = Math.cos(this.orbitPosition) * x;
-    this.sphere.position.y = Math.sin(this.orbitPosition) * y;
-    this.sphere.position.z = Math.cos(this.orbitPosition + 0) * z;
+    this.sphere.position.x = Math.cos(this.orbitalPosition) * x;
+    this.sphere.position.y = Math.sin(this.orbitalPosition) * y;
+    this.sphere.position.z = Math.cos(this.orbitalPosition + 0) * z;
   }
 }
 
