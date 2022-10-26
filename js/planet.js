e1ce1ea1910c06bfa8faf71b89acec96199fa6df
Derek McBurney <derekmcburney@Dereks-Work-MBP.localdomain>
2022-10-25 20:47:48 -0600

    Adds drawn orbits

--- js/planet.js
+++ js/planet.js
@@ -17,7 +17,7 @@ class Planet {
     if (!this.colour) this.colour = new THREE.Color( randomFromSeed()*0xffffff );
     if (!this.size) this.size = fakeGaussianRandom(-1,3)*7+1;
     if (!this.daysInAYear) this.daysInAYear = 1;
-    if (!this.orbitEccentricity) this.orbitEccentricity = fakeGaussianRandom()*0.1;
+    if (!this.orbitEccentricity) this.orbitEccentricity = fakeGaussianRandom()*0.2;
 
     this.distanceFromSun = this.minimumDistance + this.size*1.5 + fakeGaussianRandom(-9,10)*this.maximumDistance;
     this.orbitalPosition = randomFromSeed()*360;
@@ -25,11 +25,21 @@ class Planet {
   }
 
   addToScene(scene) {
-    const geometry = new THREE.SphereGeometry( this.size );
-    const material = new THREE.MeshLambertMaterial( { color: this.colour } );
-    this.sphere = new THREE.Mesh( geometry, material );
+    const sphereGeometry = new THREE.SphereGeometry( this.size );
+    const sphereMaterial = new THREE.MeshLambertMaterial( { color: this.colour } );
+    this.sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
     this.sphere.position.set( this.distanceFromSun, 0, 0);
     scene.add(this.sphere);
+
+    const lineMaterial = new THREE.LineBasicMaterial( { color: 0x333333 } );
+    const points = [];
+    for (let i=0; i < 2*Math.PI; i = i+Math.PI/32) {
+      let position = this.determinePosition(i);
+      points.push( new THREE.Vector3( position.x, position.y, position.z ) );
+    }
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
+    this.line = new THREE.Line( lineGeometry, lineMaterial );
+    scene.add(this.line);
   }
 
   nextNeighbourMinimumDistance() {
@@ -38,14 +48,25 @@ class Planet {
 
   travel() {
     this.sphere.rotation.z += this.daysInAYear*0.01;
+    this.orbitalPosition += this.speed * this.direction;
+
+    let position = this.determinePosition(this.orbitalPosition);
+
+    this.sphere.position.x = position.x;
+    this.sphere.position.y = position.y;
+    this.sphere.position.z = position.z;
+  }
+  
+  determinePosition(orbitalPosition) {
     let x = this.distanceFromSun;
     let y = this.distanceFromSun * Math.sqrt(1.0 - Math.pow(this.orbitEccentricity, 1));
     let z = Math.sin(0) * this.distanceFromSun;
-    this.orbitalPosition += this.speed * this.direction;
 
-    this.sphere.position.x = Math.cos(this.orbitalPosition) * x;
-    this.sphere.position.y = Math.sin(this.orbitalPosition) * y;
-    this.sphere.position.z = Math.cos(this.orbitalPosition + 0) * z;
+    return { 
+      x: Math.cos(orbitalPosition) * x,
+      y: Math.sin(orbitalPosition) * y,
+      z: Math.cos(orbitalPosition + 0) * z
+    }
   }
 }
 
